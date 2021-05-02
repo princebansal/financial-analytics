0899b5deb4d6069cb6e5afcd30e7b7450bd55e03
princebansal <prince.b@udaan.com>
2021-05-02 18:38:41 +0530

    Init

---
+++ hospitals-put.py
@@ -0,0 +1,39 @@
+import requests
+
+# This data was created by using the curl method explained above
+headers_list = [
+    {
+        "name": "NKS hospital location",
+        "address": "gulabi bagh",
+        "maps_link": "https://goo.gl/maps/Qk2q3dKerjthKxPV9",
+        "contact": [+911123666666],
+        "oxygen": {
+            "required": 100,
+            "current": 20,
+            "comment": "",
+            "lastUpdated": ""
+        }
+    }
+]
+
+
+url = 'https://co-ox9-default-rtdb.asia-southeast1.firebasedatabase.app/cities/-MZh0evZGFhGlnqDjFWr/hospitals.json'
+# # Get a proxy from the pool
+try:
+
+    for header in headers_list:
+        response = requests.post(url, json=header, timeout=5)
+        print(response.json())
+        # headers = random.choice(headers_list)
+        # r = requests.Session()
+        # r.headers = headers
+        # response2 = r.get(headersUrl, proxies={"http": proxy, "https": proxy}, timeout=5)
+        # print("Request #\nUser-Agent Sent:%s\n\nHeaders Recevied by HTTPBin:" %
+        #       (headers['User-Agent']))
+        # print(response2.json())
+        print("-------------------")
+except Exception as ext:
+    # Most free proxies will often get connection errors. You will have retry the entire request using another proxy to work.
+    # We will just skip retries as its beyond the scope of this tutorial and we are only downloading a single url
+    print(ext)
+    print("Skipping. Connnection error")
